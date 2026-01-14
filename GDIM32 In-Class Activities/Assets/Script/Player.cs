57ddb72b90fe06bfc13c03e04d6589da86edc27a
Denoraa <108433209+Denoraa@users.noreply.github.com>
2026-01-13 18:34:12 -0800

    scene build

---
+++ GDIM32 In-Class Activities/Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+
+
+    private void Update()
+    {
+        DetectJumpKey();
+    }
+
+
+    void DetectJumpKey()
+        {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump key pressed");
+        }
+    }
+
+}
